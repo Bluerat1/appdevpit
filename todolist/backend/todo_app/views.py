ccbcf6d8a70290ecdb099d9e6c9d022b3468d1f2
Bluerat1 <156603938+Bluerat1@users.noreply.github.com>
2025-04-02 04:35:12 +0800

    updage

--- todolist/backend/todo_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import TodoItem
-from .serializers import TodoItemSerializer
-
-class TodoItemViewSet(viewsets.ModelViewSet):
-    queryset = TodoItem.objects.all()
-    serializer_class = TodoItemSerializer
