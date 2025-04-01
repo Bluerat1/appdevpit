1123fab1b8423fab0b770d2fb202f8586dfc2406
Bluerat1 <156603938+Bluerat1@users.noreply.github.com>
2025-04-02 02:12:33 +0800

    allowedlist

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0zf6yi1rhv&&kw-slcol^630gn@he^uvl=nd*=cjg=zp44)$a9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['appdevpit.onrender.com']
 
 
 # Application definition
