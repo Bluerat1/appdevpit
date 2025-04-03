f6ec796ee3deee6da312fd9417dad0d83a24e6b7
Bluerat1 <156603938+Bluerat1@users.noreply.github.com>
2025-04-04 00:56:50 +0800

    true

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0zf6yi1rhv&&kw-slcol^630gn@he^uvl=nd*=cjg=zp44)$a9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
