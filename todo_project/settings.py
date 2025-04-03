94448ea13b14d6ddae1981011f29a96757a8c544
Bluerat1 <156603938+Bluerat1@users.noreply.github.com>
2025-04-04 00:44:14 +0800

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0zf6yi1rhv&&kw-slcol^630gn@he^uvl=nd*=cjg=zp44)$a9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['appdevpit.onrender.com/api/todos/','https://bluerat1.github.io', 'localhost']
+ALLOWED_HOSTS = ['appdevpit.onrender.com/api/todos/','https://bluerat1.github.io']
 
 CORS_ALLOW_METHODS = [
     "GET",
